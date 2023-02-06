ef5a542ca4c0c2f8d9c62978913bf684a0357ad8
Kole Tackney <45576587+KoleTackney@users.noreply.github.com>
2023-02-06 11:30:35 -0500

    Add Analytics

--- personalwebsite/src/pages/_app.tsx
+++ personalwebsite/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
+import { Analytics } from "@vercel/analytics/react";
 import { SessionProvider } from "next-auth/react";
 
 import { trpc } from "../utils/trpc";
@@ -13,6 +14,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
+      <Analytics />
     </SessionProvider>
   );
 };
