93086739da96a27aa64620889b32ccf15242e589
Kole Tackney <45576587+KoleTackney@users.noreply.github.com>
2023-05-23 14:19:31 -0400

    Fixed small errors

--- personalwebsite/src/components/timeline.tsx
+++ personalwebsite/src/components/timeline.tsx
@@ -18,15 +18,17 @@ export const TimelineCard = (
     <ol className="flex flex-col md:flex-row relative border-l border-stone-300 dark:border-stone-700">
       <li className="mb-10 ml-4">
         <div className="absolute w-3 h-3 bg-stone-300 dark:border-stone-700 rounded-full mt-2.5 -left-1.5 border border-white" />
-        <p className="flex flex-wrap gap-4 flex-row items-center justify-start">
+        <div className="flex flex-wrap gap-4 flex-row items-center justify-start">
           <span className="inline-block px-2 py-1 font-semibold bg-stone-900 dark:bg-stone-200 text-white dark:text-stone-900 rounded-md">
             {year}
           </span>
-          <h1 className="text-lg text-semibold text-stone-900 dark:text-stone-200">{name}</h1>
+          <h1 className="text-lg text-semibold text-stone-900 dark:text-stone-200">
+            {name}
+          </h1>
           <div className="my-1 text-sm font-normal text-stone-600 dark:text-stone-200">
             {duration}
           </div>
-        </p>
+        </div>
         <p className="text-sm text-stone-600 dark:text-stone-200">{details}</p>
       </li>
     </ol>
@@ -37,7 +39,7 @@ export const Timeline = ({ Items }: TimelineList) => {
   return (
     <div className="flex flex-col md:flex-row justify-center my-20">
       <div className="w-full">
-      <Title>Timeline</Title>
+        <Title>Timeline</Title>
         {Items.map(({ name, year, duration, details }) => (
           <TimelineCard
             key={name}
--- personalwebsite/src/pages/api/sendMail.ts
+++ personalwebsite/src/pages/api/sendMail.ts
@@ -2,14 +2,17 @@ import { type NextApiRequest, type NextApiResponse } from "next";
 import { mailOptions, transporter } from "../../utils/nodemailerconfig";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    if(req.method !== 'POST') {
+
+    console.log('Request received.');
+
+    if (req.method !== 'POST') {
         console.log('Only POST requests are accepted.');
         res.status(400).send('Only POST requests are accepted.');
         return;
     }
 
     const { name, email, subject, message } = req.body;
-    if(!name || !email || !subject || !message) {
+    if (!name || !email || !subject || !message) {
         console.log('Please fill in all fields.');
         res.status(400).send('Please fill in all fields.');
         return;
@@ -20,7 +23,10 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
             ...mailOptions,
             subject: `Message from ${name} - ${subject}`,
             text: message,
-            html: `<h1>Message from ${name}</h1><h2>${subject}</h2><p>${message}</p>`
+            html: `<h1>Message from ${name}</h1>
+            <h2>${subject}</h2>
+            <h2>${email}</h2>
+            <p>${message}</p>`
         });
         res.status(200).send('Message sent successfully.');
     } catch (error) {
